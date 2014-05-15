57e1652c5deb82c64aeccd26e269244f67028b7f
Paul Wells <paulwells12@gmail.com>
2014-05-15 15:10:48 -0700

    add gitignore file

---
+++ js/initialize.js
@@ -0,0 +1,13 @@
+﻿(function () {
+
+    function addListeners() {
+        document.querySelector(".itemsReorderableCheckBox").addEventListener("change", Config.itemsReorderable, true);
+        document.querySelector(".groupItemsCheckBox").addEventListener("change", Config.groupItems, true);
+        document.querySelector(".selectLayout").addEventListener("change", Config.selectLayout, true);
+        document.querySelector(".toggleOrientation").addEventListener("click", Config.toggleOrientation, true);
+    }
+
+    WinJS.Namespace.define("Init", {
+        addListeners: addListeners
+    });
+})();
